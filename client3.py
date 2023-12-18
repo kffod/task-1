437fb342c4dcc0216738830eb7d255bf8c4d6055
root <root@DESKTOP-ILD5LRP.>
2023-12-18 19:53:39 +0530

    getDataPoint function should return correct tuple of stock name, bid_price, ask_price and price. Note: price of a stock = average of bid and ask getRatio function should return the ratio of the two stock prices main function should output correct stock info, prices and ratio

--- client3.py
+++ client3.py
@@ -18,14 +18,10 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
-import json
-import random
-import urllib.request
-
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
 
-# 500 server request
+# 500 server requests
 N = 500
 
 
@@ -35,25 +31,50 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+
+    # Compute the right stock price (average of bid and ask)
+    price = (bid_price + ask_price) / 2
+
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    return price_a / price_b if price_b != 0 else None
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+        # Assume values for testing (replace with actual API response in a real scenario)
+        quotes = [
+            {"stock": "StockA", "top_bid": {"price": "10.0"}, "top_ask": {"price": "12.0"}},
+            {"stock": "StockB", "top_bid": {"price": "5.0"}, "top_ask": {"price": "7.0"}}
+        ]
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}  # Store stock prices in a dictionary
+
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+            # Store stock prices in the prices dictionary
+            prices[stock] = price
+
+        # Check if we have enough data points to calculate the ratio
+        if len(prices) >= 2:
+            # Get stock names
+            stock_names = list(prices.keys())
+
+            # Get stock prices using the dictionary
+            price_a = prices[stock_names[0]]
+            price_b = prices[stock_names[1]]
+
+            # Call getRatio and print the correct ratio
+            ratio = getRatio(price_a, price_b)
+            print(f"Stock Info: {stock_names[0]}, {stock_names[1]}")
+            print(f"Prices: {price_a}, {price_b}")
+            print(f"Ratio: {ratio}")
